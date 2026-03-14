8cf797b04299db2380d70bf4fd4c4cd7a6030394
Jordan Simonovski <jordan.simonovski@gmail.com>
2026-03-14 19:27:26 +1100

    chore(deps): resolved all outstanding security issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
@@ -11,11 +10,6 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 export default defineConfig({
   site: "https://blog.jordansimonov.ski/", // replace this with your deployed domain
   integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
     react(),
     sitemap(),
   ],
