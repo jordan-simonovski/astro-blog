2ce4b7cff961d922e6061aecec37570ef2116b07
Jordan Simonovski <jordan.simonovski@gmail.com>
2026-06-21 19:37:38 +1000

    Feat/book typeface (#56)
    * changing the typeface
    
    * fixed TOC accessibility
    
    * undo the fix to keep style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -58,6 +58,7 @@ module.exports = {
     },
     fontFamily: {
       mono: ["var(--font-mono)", "monospace"],
+      content: ["var(--font-content)", "Georgia", "Cambria", "serif"],
     },
     // },
   },
